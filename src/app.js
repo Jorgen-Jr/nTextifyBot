0c248cd118a32d4668b4e87e0ae8f06b654e4252
Jorge Antônio da Silva Nascimento Junior <jorgenjr@outlook.com>
2020-05-18 14:03:43 -0300

    :thinking:

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ const token = process.env.BOT_TOKEN;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 
-bot.answerInlineQuery(query_id, () => {
+bot.answerInlineQuery(query_id, (query_id) => {
   return [{
     message_text: query_id,
   },{
