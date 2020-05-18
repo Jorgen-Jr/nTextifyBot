9e0b84403c07bebb20c90ec3525dac025c24ff54
Jorge Antônio da Silva Nascimento Junior <jorgenjr@outlook.com>
2020-05-18 17:34:17 -0300

    Updated default reply message.

--- src/bot.js
+++ src/bot.js
@@ -70,5 +70,5 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'To use this bot, simply use the inline option @ntextifybot + text');
+  bot.sendMessage(chatId, 'To use this bot, simply use the inline option @ntextifybot + message');
 });
